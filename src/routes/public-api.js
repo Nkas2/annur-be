6e990bd2880e459d78bd13fd547ccaee1bf92fe4
Nayandra Kastoro <kastoronayandra@gmail.com>
2025-06-23 19:01:32 +0700

    add route get list transaction

--- src/routes/public-api.js
+++ src/routes/public-api.js
@@ -1,8 +1,14 @@
 import express from "express";
 import userController from "../controllers/user-controller.js";
 import eventController from "../controllers/event-controller.js";
+import transactionControllers from "../controllers/transaction-controllers.js";
 
 const publicRoutes = new express.Router();
 publicRoutes.post("/api/users/login", userController.login);
+
+publicRoutes.get(
+  "/api/list/transactions",
+  transactionControllers.getListTransactions,
+);
 
 export { publicRoutes };
