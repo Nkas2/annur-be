5dabe28d659efccf1dfaad54184c52798ebe9aa2
Nayandra Kastoro <kastoronayandra@gmail.com>
2025-07-01 16:47:26 +0700

    add event list route

--- src/validation/account-validation.js
+++ src/validation/account-validation.js
@@ -1,26 +1,26 @@
 import Joi from "joi";
 
 const createAccountValidation = Joi.object({
-    username: Joi.string().required(),
-    password: Joi.string().required().min(6),
-    email: Joi.string().email().required(),
-    name: Joi.string().required(),
-    role: Joi.number().positive().required(),
-})
+  username: Joi.string().required(),
+  password: Joi.string().required().min(6),
+  email: Joi.string().email().required(),
+  name: Joi.string().required(),
+  role: Joi.number().positive().required(),
+});
 
 const editAccountValidation = Joi.object({
-    id: Joi.number().positive().required(),
-    name: Joi.string().required(),
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
-    role: Joi.number().positive().required(),
-})
+  id: Joi.number().positive().required(),
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().optional(),
+  role: Joi.number().positive().required(),
+});
 
 const deleteAccountValidation = Joi.object({
-    id: Joi.number().positive().required(),
+  id: Joi.number().positive().required(),
 });
 export {
-    createAccountValidation,
-    editAccountValidation,
-    deleteAccountValidation
-}
+  createAccountValidation,
+  editAccountValidation,
+  deleteAccountValidation,
+};
